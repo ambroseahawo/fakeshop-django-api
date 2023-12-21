457a5f037dffaed35d87bfeedbd3bae51ef9dbb3
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 01:59:15 +0300

    Implement ProductsDetailsView

--- products/views.py
+++ products/views.py
@@ -20,3 +20,28 @@ class ProductsListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProductDetailView(APIView):
+    def get_object(self, pk):
+        product = get_object_or_404(Product, id=pk)
+        return product
+    
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(data={"detail": "Deleted Successfully"},status=status.HTTP_204_NO_CONTENT)
