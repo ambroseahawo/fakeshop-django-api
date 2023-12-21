f927250d8df2f108d3a58705d5f081d9c0d60ca8
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 01:08:55 +0300

    Implement category model

--- products/models.py
+++ products/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(unique=True, blank=False, null=False, max_length=50)
+    
+
 class Product(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=50)
     description = models.CharField(blank=False, null=False, max_length=250)
-    category = models.CharField(blank=False, null=False)
+    # category = models.CharField(blank=False, null=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=False, null=False)
     price = models.PositiveIntegerField(blank=False, null=False)
