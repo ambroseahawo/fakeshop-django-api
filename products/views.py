1a298efa15acde410f53fc60f114c227557b6a19
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 01:48:05 +0300

    Implement ProductsListView

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from products.models import Product
+from products.serializers import ProductSerializer
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class ProductsListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
