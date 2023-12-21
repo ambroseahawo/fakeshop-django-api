4ebcb6099ffb59d55d81fa2a02a313aca5cc01b1
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 00:54:46 +0300

    Implement Product Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(unique=True, blank=False, null=False, max_length=50)
+    description = models.CharField(blank=False, null=False, max_length=250)
+    category = models.CharField(blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
