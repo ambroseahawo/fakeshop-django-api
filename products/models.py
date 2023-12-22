9cbc080af6e5dd9fe81513fba6d73d0c11294e6e
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 13:03:25 +0300

    Alter Product name max_length

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=50)
-    description = models.CharField(blank=False, null=False, max_length=250)
+    description = models.CharField(blank=False, null=False, max_length=500)
     # category = models.CharField(blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=False, null=False)
     price = models.PositiveIntegerField(blank=False, null=False)
