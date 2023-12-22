0c78e7adbf5c37c276ab01628867f10a3398ec29
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 13:31:54 +0300

    Implement Category Detail View

--- categories/views.py
+++ categories/views.py
@@ -20,3 +20,28 @@ class CategoriesListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoryDetailView(APIView):
+    def get_object(self, pk):
+        category = get_object_or_404(Category, id=pk)
+        return category
+    
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(data={"detail": "Deleted Successfully"}, status=status.HTTP_204_NO_CONTENT)
