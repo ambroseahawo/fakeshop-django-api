c942bb07573d24e2abf94834f3721075b2590e26
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-22 13:24:44 +0300

    Implement Categories List View

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from categories.models import Category
+from categories.serializers import CategorySerializer
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class CategoriesListView(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
