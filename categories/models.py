b4a8b154e665fda58631708abd33fea0aabe997b
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-26 11:40:08 +0300

    update Category model plural name

--- categories/models.py
+++ categories/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=50)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
