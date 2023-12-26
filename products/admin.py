e2eec49e09b6b40744715c601dca7cb7e5b1538e
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-26 11:48:01 +0300

    Implement Product admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from products.models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductModel(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'id')
+    list_filter = ('name', 'category', 'price')
+    search_field = ('name', 'category', 'price')
